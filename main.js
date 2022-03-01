5f8a83d8b46ccfee382ed469f96d6bc6443dacfd
Jerry <hei.jerry.2@gmail.com>
2022-03-01 12:55:34 +0300

    v1

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const bubble = document.createElement('span');
-bubble.classList.add('bubble')
-document.body.appendChild(bubble)
+let bubbleMaker = () => {
+    const bubble = document.createElement('span');
+    bubble.classList.add('bubble')
+    document.body.appendChild(bubble)
 
-const size = Math.random()*200+100+"px" ;
-bubble.style.height = size ;
-bubble.style.width = size ;
+    const size = Math.random()*200+100+"px" ;
+    bubble.style.height = size ;
+    bubble.style.width = size ;
 
-bubble.style.top = Math.random()*150+"%";
-bubble.style.left = Math.random()*100+"%";
+    bubble.style.top = Math.random()*150+"%";
+    bubble.style.left = Math.random()*100+"%";
 
+    bubble.style.setProperty('--left', Math.random()*100+"%");
+};
+
+setInterval(bubbleMaker,300)
